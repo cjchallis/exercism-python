edb336f38769adbf1490eac3a82afe3d9d203d1e
cjchallis <christopher.challis@gmail.com>
2015-03-20 16:11:25 +0000

    Capture last two commits.

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -3,9 +3,17 @@ UNEQUAL = 0
 SUBLIST = 1
 SUPERLIST = 2
 
+def sub_list(list1, list2):
+	if not list1:
+		return True
+	while list1[0] in list2:
+		i = list2.index(list1[0])
+		if list2[i:i+len(list1)] == list1:
+			return True
+		list2 = list2[i+1:]	
+	return False
+	
 def check_lists(list1, list2):
-    set1 = set(list1)
-    set2 = set(list2)
-    sub = all([s in set2 for s in set1])
-    sup = all([s in set1 for s in set2])
-    return sub + 2*sup
+	sub = sub_list(list1, list2)
+	sup = sub_list(list2, list1)
+	return sub + 2*sup
