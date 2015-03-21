1e6ff43713380ffa7c1c580e7e717fdc27a1a8ff
cjchallis <christopher.challis@gmail.com>
2015-03-21 14:46:22 +0000

    Update roman numeral exercise.

--- roman-numerals/roman.py
+++ roman-numerals/roman.py
@@ -1,23 +1,14 @@
 translate = {
-             0: '',
-             1: 'I',
-             5: 'V',
-             10: 'X',
-             50: 'L',
-             100: 'C',
-             500: 'D',
+             1:'I', 4:'IV', 5:'V', 9:'IX',
+             10:'X', 40:'XL', 50:'L', 90:'XC',
+             100:'C', 400:'CD', 500:'D', 900:'CM',
              1000: 'M'
              }
- 
-def digit(digit, pos):
-	digit = int(digit)
-	pow = 10 ** pos
-	if digit % 5 == 4:
-		return translate[pow] + translate[pow * (digit+1)]
-	else:
-		first = translate[0 if digit < 5 else 5 * pow]
-		return first + translate[pow] * (digit % 5)
-		
+
 def numeral(arabic):
-	text = '%04d' % (arabic)
-	return ''.join([digit(text[i], 3-i) for i in range(4)])
+	roman = ''
+	for v in sorted(translate, reverse=True):
+		while arabic >= v:
+			roman += translate[v]
+			arabic -= v
+	return roman
