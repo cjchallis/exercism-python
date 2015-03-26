3981d2f6a53a286d4d30ed595a816f30546c9f6d
cjchallis <christopher.challis@gmail.com>
2015-03-26 18:46:39 +0000

    First solution to binary exercise.

--- binary/binary.py
+++ binary/binary.py
@@ -2,4 +2,4 @@ def parse_binary(bin):
 	if bin.strip('01'):
 		raise ValueError('Input not binary')
 	n = len(bin)
-	return sum([2**(n-i-1) for i in range(n) if bin[i] == '1'])
+	return sum([1<<(n-i-1) for i in range(n) if bin[i] == '1'])
