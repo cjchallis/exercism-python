41c5041ae2179d0fdee059f56d3081d42c91a9bf
cjchallis <christopher.challis@gmail.com>
2015-03-30 11:27:42 -0400

    Better error handling in secret handshake.
    Also additional global constants to avoid some hard-coded
    in-line values.

--- secret-handshake/handshake.py
+++ secret-handshake/handshake.py
@@ -1,29 +1,32 @@
 actions = {1:'wink', 2:'double blink', 4:'close your eyes', 8:'jump'}
 codes = {v:k for k,v in actions.items()}
 A = len(actions)
+REV = 2 ** A
+MAX = 2 ** (A+1)
 
 def handshake(n):
-	try:
-		n = int(n, 2)
-	except (TypeError, ValueError):
-		pass
-	try:
-		if not 0 < n < 2 ** (A + 1):
-			return []
-		shake = [actions[2 ** i] for i in range(A) if 2 ** i & n]
-		if 2 ** A & n:
-			shake.reverse()
-		return shake
-	except ValueError:
-		return []
-	
+    try:
+        n = int(n, 2)
+    except ValueError:
+        return []
+    except TypeError:
+        pass
+#    try:
+    if not 0 < n < MAX:
+        return []
+    shake = [actions[a] for a in sorted(actions) if a & n]
+    if REV & n:
+        shake.reverse()
+    return shake
+    #except (TypeError, ValueError):
+     #   return []
+    
 def code(shake):
-	try:
-		num = sum([codes[s] for s in shake])
-		if len(shake) > 1:
-			if codes[shake[0]] > codes[shake[1]]:
-				num += 2 ** A
-	except KeyError:
-		num = 0
-	return "{0:b}".format(num)
-	
+    try:
+        num = sum([codes[s] for s in shake])
+        if len(shake) > 1 and codes[shake[0]] > codes[shake[1]]:
+            num += REV
+    except KeyError:
+        num = 0
+    return "{0:b}".format(num)
+    
