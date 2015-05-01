569978fd0ef83ed3e0c49b3fe67fce71fbb95a00
cjchallis <christopher.challis@gmail.com>
2015-05-01 17:02:01 -0400

    Condense repeated condition in prime factors

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,13 +1,12 @@
 def prime_factors(n):
     factors = []
-    if n > 1:
-        next(n, factors)
-    return factors
+    return next(n, factors)
 	
 def next(k, factors):
+    if k <= 1:
+        return factors
     i = 2
     while k % i:
         i += 1
     factors.append(i)
-    if k / i > 1:
-        next(k / i, factors)
+    return next(k / i, factors)
