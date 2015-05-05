ddfee5f93f6fcc24878be0f2e2075a1881fd751d
cjchallis <christopher.challis@gmail.com>
2015-05-05 13:30:46 -0400

    Further condense prime factors solution

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,11 +1,9 @@
 def prime_factors(n):
-    factors = []
-    return next(n, factors, 2)
+    return next(n, [], 2)
 	
 def next(k, factors, p):
     if k <= 1:
         return factors
     while k % p:
         p += 1
-    factors.append(p)
-    return next(k / p, factors, p)
+    return next(k / p, factors + [p], p)
