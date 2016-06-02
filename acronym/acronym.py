a902471144a0795f59f724a8255e3a344033eb9d
Chris Challis <chris.j.challis@huntington.com>
2016-06-02 08:15:55 -0700

    Update acronym to not exceed line length 80

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,4 +1,4 @@
-import re
+from re import split
 
 def abbreviate(string):
-    return ''.join([w[0].upper() for w in re.split(" |-|[a-z](?=[A-Z])", string)])
+    return ''.join([w[0].upper() for w in split(" |-|[a-z](?=[A-Z])", string)])
