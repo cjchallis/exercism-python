b51724bf82a04028a32aa4cc45562d8df61135b6
Chris Challis <chris.j.challis@huntington.com>
2016-06-03 12:21:10 -0700

    First solution to say exercise

--- say/say.py
+++ say/say.py
@@ -1,20 +1,45 @@
-# coding: utf-8
-def say(num):
-    orders = ['trillion', 'billion', 'million', 'thousand', '']
-    while num > 0:
-        rmdr = num % 1000
-        num = (num - rmdr) / 1000
-        
-ONES = dict(zip(range(20), ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']))
+import re
 
-TENS = dict(zip(range(2,10),['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']))
+ONES = dict(zip(range(20),
+                ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+                 'eight', 'nine','ten','eleven','twelve','thirteen','fourteen',
+                 'fifteen', 'sixteen','seventeen','eighteen','nineteen']))
+TENS = dict(zip(range(2,10),
+                ['twenty','thirty','forty','fifty','sixty','seventy','eighty',
+                 'ninety']))
+ORDERS = ['', 'thousand', 'million', 'billion']
 
-def _three_digit(num):
-    pass
-    
-def _two_digit(num):
-    if num < 20:
-        return ONES[num]
-    
-    return 
-    
+def say(n):
+    if not 0 <= n < 1e12:
+        raise AttributeError('Value must be between 0 and 999,999,999,999')
+    if n == 0:
+        return 'zero'
+    out = [] 
+    i = 0
+    while n > 0:
+        n, r = divmod(n, 1000)
+        if r > 0:
+            out.insert(0, ORDERS[i])
+        out[0:0] = _three_digit(r)
+        if i == 0 and 0 < r < 100 and n > 0:
+            out.insert(0, 'and')
+        i += 1                
+    return re.sub(' +', ' ', ' '.join(out)).strip()
+
+
+def _three_digit(n):
+    three, two = divmod(n, 100)
+    return [ONES[three], filler('hundred', three), filler('and', three * two),
+            _two_digit(two)]
+
+
+def _two_digit(n):
+    if n < 20:
+        return ONES[n]
+    two, one = divmod(n, 10)
+    return '{0}{1}{2}'.format(TENS[two], filler('-', one), ONES[one])
+
+
+def filler(word, n):
+    return  word * (n > 0)
+
